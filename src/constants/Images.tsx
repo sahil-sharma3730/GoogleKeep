c0860c11a46dd6f8d8996118d533dc1d5970359b
sahil-sharma3730 <sahilsharma3730@gmail.com>
2023-12-18 23:08:18 +0530

    initial commit

---
+++ src/constants/Images.tsx
@@ -0,0 +1,6 @@
+export const Images = {
+  colorPicker: require('../assets/colorPicker.png'),
+  back: require('../assets/back.png'),
+  tick: require('../assets/tick.png'),
+  close: require('../assets/cancel.jpeg'),
+};
