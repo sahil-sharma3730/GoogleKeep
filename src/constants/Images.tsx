c8e0cd7418cc7b733b8bb80f31b1b2fc4fe8a0de
sahil-sharma3730 <sahilsharma3730@gmail.com>
2023-12-18 23:44:02 +0530

    color changes

--- src/constants/Images.tsx
+++ src/constants/Images.tsx
@@ -1,6 +1,6 @@
 export const Images = {
-  colorPicker: require('../assets/colorPicker.png'),
-  back: require('../assets/back.png'),
-  tick: require('../assets/tick.png'),
-  close: require('../assets/cancel.jpeg'),
+  colorPicker: require("../assets/colorPicker.png"),
+  back: require("../assets/back.png"),
+  tick: require("../assets/tick.png"),
+  cancel: require("../assets/cancel.png"),
 };
